3b3dbe0e7cfd49dfbeb923da84901b79fe390d0a
Dornex <43918692+Dornex@users.noreply.github.com>
2019-11-02 20:21:53 +0200

    Lab 2 update

--- Laboratory homeworks/README.md
+++ Laboratory homeworks/README.md
@@ -1,1 +1,14 @@
-# Temp title
+# Robotics laboratory homeworks
+
+## analogRGBControl.ino
+
+RGB Led that is controlled using 3 potentiometers (1 for red, 1 for green and 1 for blue).
+
+Requirments:
+
+1. 3 potentiometers
+2. RGB Led
+3. 3 resistors (220 ohm)
+4. wires
+5. Arduino
+6. Connector cable
---
+++ Laboratory homeworks/analogRGBControl.ino
@@ -0,0 +1,26 @@
+const int redPin = 9, greenPin = 10, bluePin = 11;
+const int redPot = A0, greenPot = A1, bluePot = A2;
+int analogRedValue, analogGreenValue, analogBlueValue;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  analogRedValue = analogRead(redPot);
+  analogGreenValue = analogRead(greenPot);
+  analogBlueValue = analogRead(bluePot);
+  
+  setColor(analogRedValue, analogGreenValue, analogBlueValue);
+  
+  delay(1); 
+}
+
+void setColor(int red, int green, int blue) {
+  analogWrite(redPin, map(red, 0, 1023, 0 , 255));
+  analogWrite(greenPin, map(green, 0, 1023, 0 , 255));
+  analogWrite(bluePin, map(blue, 0, 1023, 0 , 255));
+}
